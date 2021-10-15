13dfc10363f1ac77794be820575f2c428c148820
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-10-15 23:53:05 +0900

    added background.js

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
+window.bears = {}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  window.bears[request.url] = request.count
+})
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.create({ url: 'popup.html' })
+})
--- popup.js
+++ popup.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', onclick, false)
+  const background = chrome.extension.getBackgroundPage()
 
-  function onclick () {
-    chrome.tabs.query({ currentWindow: true, active: true }, 
-    function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, 'hi!!', setCount)
-    })
-  }
+  Object.keys(background.bears).forEach(function (url) {
+    const div = document.createElement('div')
+    div.textContent = `${url}: ${background.bears[url]}`
+    document.body.appendChild(div)
+  })
+
+  // document.querySelector('button').addEventListener('click', onclick, false)
+
+  // function onclick () {
+  //   chrome.tabs.query({ currentWindow: true, active: true }, 
+  //   function (tabs) {
+  //     chrome.tabs.sendMessage(tabs[0].id, 'hi!!', setCount)
+  //   })
+  // }
 }, false)
 
-function setCount (res) {
-  const div = document.createElement('div')
-  div.textContent = `${res.count} bears`
-  document.body.appendChild(div)
-}
+// function setCount (res) {
+//   const div = document.createElement('div')
+//   div.textContent = `${res.count} bears`
+//   document.body.appendChild(div)
+// }
